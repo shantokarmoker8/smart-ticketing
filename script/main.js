131c02fc10ec91f7d21226c3b18be72f620939ca
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-02-28 18:02:32 +0600

    js sits

---
+++ script/main.js
@@ -0,0 +1,282 @@
+const sitA1 = document.getElementById("a1");
+sitA1.addEventListener("click", function () {
+  document.getElementById("a1").classList.add("bg-[#1DD100]");
+  document.getElementById("a1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "A1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+
+const sitA2 = document.getElementById("a2");
+sitA2.addEventListener("click", function () {
+  document.getElementById("a2").classList.add("bg-[#1DD100]");
+  document.getElementById("a2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "A2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitA3 = document.getElementById("a3");
+sitA3.addEventListener("click", function () {
+  document.getElementById("a3").classList.add("bg-[#1DD100]");
+  document.getElementById("a3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "A3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitA4 = document.getElementById("a4");
+sitA4.addEventListener("click", function () {
+  document.getElementById("a4").classList.add("bg-[#1DD100]");
+  document.getElementById("a4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "A4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitB1 = document.getElementById("b1");
+sitB1.addEventListener("click", function () {
+  document.getElementById("b1").classList.add("bg-[#1DD100]");
+  document.getElementById("b1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "B1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitB2 = document.getElementById("b2");
+sitB2.addEventListener("click", function () {
+  document.getElementById("b2").classList.add("bg-[#1DD100]");
+  document.getElementById("b2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "B2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitB3 = document.getElementById("b3");
+sitB3.addEventListener("click", function () {
+  document.getElementById("b3").classList.add("bg-[#1DD100]");
+  document.getElementById("b3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "B3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitB4 = document.getElementById("b4");
+sitB4.addEventListener("click", function () {
+  document.getElementById("b4").classList.add("bg-[#1DD100]");
+  document.getElementById("b4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "B4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitC1 = document.getElementById("c1");
+sitC1.addEventListener("click", function () {
+  document.getElementById("c1").classList.add("bg-[#1DD100]");
+  document.getElementById("c1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "C1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitC2 = document.getElementById("c2");
+sitC2.addEventListener("click", function () {
+  document.getElementById("c2").classList.add("bg-[#1DD100]");
+  document.getElementById("c2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "C2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitC3 = document.getElementById("c3");
+sitC3.addEventListener("click", function () {
+  document.getElementById("c3").classList.add("bg-[#1DD100]");
+  document.getElementById("c3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "C3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitC4 = document.getElementById("c4");
+sitC4.addEventListener("click", function () {
+  document.getElementById("c4").classList.add("bg-[#1DD100]");
+  document.getElementById("c4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "C4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitD1 = document.getElementById("d1");
+sitD1.addEventListener("click", function () {
+  document.getElementById("d1").classList.add("bg-[#1DD100]");
+  document.getElementById("d1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "D1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitD2 = document.getElementById("d2");
+sitD2.addEventListener("click", function () {
+  document.getElementById("d2").classList.add("bg-[#1DD100]");
+  document.getElementById("d2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "D2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitD3 = document.getElementById("d3");
+sitD3.addEventListener("click", function () {
+  document.getElementById("d3").classList.add("bg-[#1DD100]");
+  document.getElementById("d3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "D3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitD4 = document.getElementById("d4");
+sitD4.addEventListener("click", function () {
+  document.getElementById("d4").classList.add("bg-[#1DD100]");
+  document.getElementById("d4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "D4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitE1 = document.getElementById("e1");
+sitE1.addEventListener("click", function () {
+  document.getElementById("e1").classList.add("bg-[#1DD100]");
+  document.getElementById("e1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "E1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitE2 = document.getElementById("e2");
+sitE2.addEventListener("click", function () {
+  document.getElementById("e2").classList.add("bg-[#1DD100]");
+  document.getElementById("e2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "E2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitE3 = document.getElementById("e3");
+sitE3.addEventListener("click", function () {
+  document.getElementById("e3").classList.add("bg-[#1DD100]");
+  document.getElementById("e3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "E3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitE4 = document.getElementById("e4");
+sitE4.addEventListener("click", function () {
+  document.getElementById("e4").classList.add("bg-[#1DD100]");
+  document.getElementById("e4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "E4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+
+const sitF1 = document.getElementById("f1");
+sitF1.addEventListener("click", function () {
+  document.getElementById("f1").classList.add("bg-[#1DD100]");
+  document.getElementById("f1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "F1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitF2 = document.getElementById("f2");
+sitF2.addEventListener("click", function () {
+  document.getElementById("f2").classList.add("bg-[#1DD100]");
+  document.getElementById("f2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "F2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitF3 = document.getElementById("f3");
+sitF3.addEventListener("click", function () {
+  document.getElementById("f3").classList.add("bg-[#1DD100]");
+  document.getElementById("f3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "F3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitF4 = document.getElementById("f4");
+sitF4.addEventListener("click", function () {
+  document.getElementById("f4").classList.add("bg-[#1DD100]");
+  document.getElementById("f4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "F4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitG1 = document.getElementById("g1");
+sitG1.addEventListener("click", function () {
+  document.getElementById("g1").classList.add("bg-[#1DD100]");
+  document.getElementById("g1").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "G1";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitG2 = document.getElementById("g2");
+sitG2.addEventListener("click", function () {
+  document.getElementById("g2").classList.add("bg-[#1DD100]");
+  document.getElementById("g2").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "G2";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitG3 = document.getElementById("g3");
+sitG3.addEventListener("click", function () {
+  document.getElementById("g3").classList.add("bg-[#1DD100]");
+  document.getElementById("g3").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "G3";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
+const sitG4 = document.getElementById("g4");
+sitG4.addEventListener("click", function () {
+  document.getElementById("g4").classList.add("bg-[#1DD100]");
+  document.getElementById("g4").classList.remove("bg-[#F7F8F8]");
+  document.getElementById("seat").innerText = "G4";
+  document.getElementById("class").innerText = "Economoy";
+  document.getElementById("price").innerText = 550;
+  document.getElementById("total").innerText = 550;
+  document.getElementById("grend-total").innerText = 500;
+});
